ca975e24a9662315a10af0b34e670ef572cc1cdf
Gaspar Tartari <gaspartartari@gmail.com>
2024-07-28 15:42:49 -0300

    other custom button click events

--- src/components/ScheduleMeeting/index.tsx
+++ src/components/ScheduleMeeting/index.tsx
@@ -7,7 +7,15 @@ export default function ScheduleMeeting() {
                 <h2>Agende uma Reunião Gratuita</h2>
                 <p>Converse conosco por 30 minutos e descubra como podemos ajudar o seu negócio a crescer.</p>
                 <div className='lp-schedule-button-container'>
-                    <a href='https://calendly.com/gaspartartari' target='_blank' rel='noopener noreferrer' className='lp-schedule-button'>
+                    <a 
+                    href='https://calendly.com/gaspartartari' 
+                    target='_blank' 
+                    rel='noopener noreferrer'
+                    onClick={() => {
+                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                        (window as any).dataLayer.push({ 'event': 'schedule_button' });
+                    }} 
+                    className='lp-schedule-button'>
                         Agendar Agora
                     </a>
                 </div>
--- src/components/WhatsAppButtonProps/index.tsx
+++ src/components/WhatsAppButtonProps/index.tsx
@@ -11,7 +11,16 @@ const WhatsAppButton: React.FC<WhatsAppButtonProps> = ({ number, text, message }
     const link = `https://wa.me/${number}?text=${encodeURIComponent(message)}`;
     return (
         <div className='lp-whatsapp-button-container'>
-            <a href={link} target='_blank' rel='noopener noreferrer' className='lp-whatsapp-button lp-container'>
+            <a 
+            href={link} 
+            target='_blank' 
+            rel='noopener noreferrer' 
+            className='lp-whatsapp-button lp-container'
+            onClick={() => {
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                (window as any).dataLayer.push({ 'event': 'schedule_button' });
+            }}
+            >
                 {text}
             </a>
 
